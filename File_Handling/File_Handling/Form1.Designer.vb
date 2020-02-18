90587dbe967fa4aa8c1731d2edf9c792917af54c
markfausty <59746144+markfausty@users.noreply.github.com>
2020-02-19 02:54:08 +0800

    Add files via upload

---
+++ File_Handling/File_Handling/Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.tbName = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.tbAddress = New System.Windows.Forms.TextBox()
+        Me.tbAge = New System.Windows.Forms.TextBox()
+        Me.btnTxt = New System.Windows.Forms.Button()
+        Me.btnJson = New System.Windows.Forms.Button()
+        Me.btnXml = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'tbName
+        '
+        Me.tbName.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbName.Location = New System.Drawing.Point(171, 64)
+        Me.tbName.Margin = New System.Windows.Forms.Padding(2)
+        Me.tbName.Multiline = True
+        Me.tbName.Name = "tbName"
+        Me.tbName.Size = New System.Drawing.Size(205, 24)
+        Me.tbName.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Constantia", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(68, 67)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 23)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Constantia", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(68, 130)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 23)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Address"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Constantia", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(68, 98)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 23)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Age"
+        '
+        'tbAddress
+        '
+        Me.tbAddress.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbAddress.Location = New System.Drawing.Point(171, 130)
+        Me.tbAddress.Margin = New System.Windows.Forms.Padding(2)
+        Me.tbAddress.Multiline = True
+        Me.tbAddress.Name = "tbAddress"
+        Me.tbAddress.Size = New System.Drawing.Size(205, 24)
+        Me.tbAddress.TabIndex = 4
+        '
+        'tbAge
+        '
+        Me.tbAge.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbAge.Location = New System.Drawing.Point(171, 98)
+        Me.tbAge.Margin = New System.Windows.Forms.Padding(2)
+        Me.tbAge.Multiline = True
+        Me.tbAge.Name = "tbAge"
+        Me.tbAge.Size = New System.Drawing.Size(60, 24)
+        Me.tbAge.TabIndex = 5
+        '
+        'btnTxt
+        '
+        Me.btnTxt.Font = New System.Drawing.Font("Constantia", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTxt.Location = New System.Drawing.Point(82, 188)
+        Me.btnTxt.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnTxt.Name = "btnTxt"
+        Me.btnTxt.Size = New System.Drawing.Size(70, 37)
+        Me.btnTxt.TabIndex = 7
+        Me.btnTxt.Text = "TXT"
+        Me.btnTxt.UseVisualStyleBackColor = True
+        '
+        'btnJson
+        '
+        Me.btnJson.Font = New System.Drawing.Font("Constantia", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnJson.Location = New System.Drawing.Point(286, 188)
+        Me.btnJson.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnJson.Name = "btnJson"
+        Me.btnJson.Size = New System.Drawing.Size(70, 37)
+        Me.btnJson.TabIndex = 8
+        Me.btnJson.Text = "JSON"
+        Me.btnJson.UseVisualStyleBackColor = True
+        '
+        'btnXml
+        '
+        Me.btnXml.Font = New System.Drawing.Font("Constantia", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnXml.Location = New System.Drawing.Point(185, 188)
+        Me.btnXml.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnXml.Name = "btnXml"
+        Me.btnXml.Size = New System.Drawing.Size(70, 37)
+        Me.btnXml.TabIndex = 9
+        Me.btnXml.Text = "XML"
+        Me.btnXml.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(450, 275)
+        Me.Controls.Add(Me.btnXml)
+        Me.Controls.Add(Me.btnJson)
+        Me.Controls.Add(Me.btnTxt)
+        Me.Controls.Add(Me.tbAge)
+        Me.Controls.Add(Me.tbAddress)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.tbName)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "Form1"
+        Me.Text = "File Handling"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents tbName As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents tbAddress As TextBox
+    Friend WithEvents tbAge As TextBox
+    Friend WithEvents btnTxt As Button
+    Friend WithEvents btnJson As Button
+    Friend WithEvents btnXml As Button
+End Class
---
+++ File_Handling/File_Handling/Form1.vb
@@ -0,0 +1,175 @@
+﻿Imports System.IO
+Imports System.Xml
+Imports Newtonsoft.Json
+
+Public Class Form1
+
+    Dim name1 As String
+    Dim age As String
+    Dim Address As String
+    Dim file_name As String
+
+    Dim ser As JsonSerializer = New JsonSerializer()
+
+    Private Sub tbName_KeyPress(Sender As Object, e As KeyPressEventArgs) Handles tbName.KeyPress
+
+        e.Handled = True
+
+        If e.KeyChar Like "[A-z]" Or e.KeyChar = "." Or e.KeyChar = Chr(8) Or e.KeyChar = Chr(32) Then
+
+            e.Handled = False
+
+        End If
+
+    End Sub
+
+    Private Sub tbAge_KeyPress(Sender As Object, e As KeyPressEventArgs) Handles tbAge.KeyPress
+
+        e.Handled = True
+
+        If IsNumeric(e.KeyChar) Or e.KeyChar = Chr(8) Then
+
+            e.Handled = False
+
+        End If
+
+    End Sub
+    Private Sub tbAddress_KeyPress(Sender As Object, e As KeyPressEventArgs) Handles tbAddress.KeyPress
+
+        e.Handled = True
+
+        If e.KeyChar Like "[A-z]" Or e.KeyChar = "." Or e.KeyChar = "," Or e.KeyChar = Chr(8) Or e.KeyChar = Chr(32) Then
+
+            e.Handled = False
+
+        End If
+
+    End Sub
+
+    Private Sub btnTxt_Click(sender As Object, e As EventArgs) Handles btnTxt.Click
+
+        If tbAge.Text.Length >= 255 Then
+
+            MessageBox.Show("Youhave exceeded the maximum value of 255")
+
+        End If
+
+        name1 = tbName.Text
+        age = tbAge.Text
+        Address = tbAddress.Text
+
+        file_name = name1 + ".txt"
+
+        Try
+
+            Using file_write As StreamWriter = New StreamWriter(file_name)
+
+                file_write.WriteLine("Name: " + name1)
+                file_write.WriteLine("Age: " + age)
+                file_write.WriteLine("Address: " + Address)
+
+            End Using
+
+            MessageBox.Show("TXT file saved")
+
+        Catch ex As Exception
+
+            Dim error_key As String = ex.ToString
+            MessageBox.Show("The file can't be processed " + error_key)
+
+        End Try
+
+    End Sub
+
+    Private Sub btnXml_Click(sender As Object, e As EventArgs) Handles btnXml.Click
+
+        If tbAge.Text.Length >= 255 Then
+
+            MessageBox.Show("Youhave exceeded the maximum value of 255")
+
+        End If
+
+        file_name = tbName.Text + ".xml"
+
+        Dim settings As New XmlWriterSettings()
+        settings.Indent = True
+
+        ' Initialize the XmlWriter
+        Dim xml_write As XmlWriter = XmlWriter.Create(file_name, settings)
+
+        With xml_write
+
+            ' Write the xml declaration
+            .WriteStartDocument()
+
+            ' Write a comment
+            .WriteComment("XML Data")
+
+            ' Write the root element
+            ' .WriteStartElement("Data")
+
+            ' Start first person
+            .WriteStartElement("Person")
+
+            ' Person node
+            .WriteStartElement("Name")
+            .WriteString(tbName.Text.ToString)
+            .WriteEndElement()
+
+            .WriteStartElement("Age")
+            .WriteString(tbAge.Text.ToString)
+            .WriteEndElement()
+
+            .WriteStartElement("Address")
+            .WriteString(tbAddress.Text.ToString)
+            .WriteEndElement()
+
+            ' End of person
+            .WriteEndElement()
+
+            ' Close the XmlTextWriter
+            .WriteEndDocument()
+            .Close()
+
+        End With
+
+        MessageBox.Show("XML file saved")
+
+    End Sub
+
+    Private Sub btnJson_Click(sender As Object, e As EventArgs) Handles btnJson.Click
+
+        ' Create Person class
+        Dim person As Person = New Person()
+
+        person.Name = tbName.Text
+        person.Age = tbAge.Text
+        person.Address = tbAddress.Text
+
+        file_name = tbName.Text + ".json"
+
+        Dim json_write As String = JsonConvert.SerializeObject(person)
+        File.WriteAllText(file_name, json_write)
+
+        MessageBox.Show("JSON file saved")
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub tbName_TextChanged(sender As Object, e As EventArgs) Handles tbName.TextChanged
+
+    End Sub
+End Class
+
+Class Person
+
+    Public Property Name As String
+    Public Property Age As String
+    Public Property Address As String
+
+End Class
+
+
